0e9936cc0f75d4671d973314603ad3d79353711a
Sako Koji <sako.koji@gmail.com>
2018-11-15 17:03:47 +0900

    それなりにできた

--- convtw.py
+++ convtw.py
@@ -1,4 +1,27 @@
+#!/usr/bin/env python
 # Pillowを使用した画像圧縮プログラム
-# -Wオプションで横サイズを指定する機能をつける
+from PIL import Image,ImageFilter
+import sys
+import os
 
-from PIL import image,imageFilter
+quality = 75
+width = 1980
+args = sys.argv 
+
+def conv(InputFile,OutputFile):
+    try:
+        im = Image.open(InputFile)
+    except IOError:
+        pass
+    
+    resize_im = im.resize((width,int(width / im.width * im.height)),Image.LANCZOS)
+    resize_im.save(OutputFile, quality=quality)
+
+def GenerateOutputFileName(InputFile):
+    return 'TW_' + InputFile
+
+if __name__ == "__main__":
+    args.pop(0)
+    for arg in args:
+        if os.path.isfile(arg):
+            conv(arg,GenerateOutputFileName(arg))   
